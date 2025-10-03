f1b14702fde4f0c2c35b3f4d9a1a3264ca348dbe
Danail Donkov <danail.donkov5@gmail.com>
2025-10-03 23:28:59 +0300

    Update Recursive_Power.py

--- Python_Advanced/Functions_Advanced/Recursive_Power.py
+++ Python_Advanced/Functions_Advanced/Recursive_Power.py
@@ -1,3 +1,12 @@
 #1
 def recursive_power(number, power):
     return number ** power
+
+#2
+def recursive_power(number, power):
+    if power == 0:
+        return 1
+    return number * recursive_power(number, power - 1)
+
+print(recursive_power(2, 10))
+print(recursive_power(10, 100))
