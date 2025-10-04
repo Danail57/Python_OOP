0c784c4430a442d5b1468876f84d85e2611d0b67
Danail Donkov <danail.donkov5@gmail.com>
2025-10-04 19:05:55 +0300

    Update So_Many_Exceptions.py

--- Python_Advanced/Error_Handling/So_Many_Exceptions.py
+++ Python_Advanced/Error_Handling/So_Many_Exceptions.py
@@ -1,1 +1,11 @@
+numbers_list = [int(element) for element in input().split(", ")]
+result = 1
 
+for i in range(len(numbers_list)):
+    number = numbers_list[i]
+    if number <= 5:
+        result *= number
+    elif 5 < number <= 10:
+        result /= number
+
+print(result)
