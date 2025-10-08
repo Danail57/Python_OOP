8233be55220236af4c81c293c6ba1a37ab3a8668
Danail Donkov <danail.donkov5@gmail.com>
2025-10-08 11:31:54 +0300

    Update File_Writer.py

--- Python_Advanced/File_Handling/File_Writer.py
+++ Python_Advanced/File_Handling/File_Writer.py
@@ -1,1 +1,17 @@
+import os
 
+path = os.path.join(os.getcwd(), 'writing_file.txt')
+with open(path, 'w', encoding='utf-8') as f:
+    numbers = [10, 20, 30, 40, 50, 60, 70, 85]
+    for number in numbers:
+        f.write(str(number) + '\n')
+print('Numbers exist.')
+
+with open(path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    numbers = [int(line.strip()) for line in lines]
+    total = sum(numbers)
+    average = total / len(numbers)
+print('Numbers are:', numbers)
+print('Total:', total)
+print(f'Average:, {average:.2f}')
