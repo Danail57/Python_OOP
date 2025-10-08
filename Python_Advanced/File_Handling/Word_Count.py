b9c7fffe5d426f459e738faaa0bcca0e129f9302
Danail Donkov <danail.donkov5@gmail.com>
2025-10-08 11:40:44 +0300

    Update Word_Count.py

--- Python_Advanced/File_Handling/Word_Count.py
+++ Python_Advanced/File_Handling/Word_Count.py
@@ -1,1 +1,9 @@
+import os
+
+path = os.path.join(os.getcwd(), 'word_counnt.txt')
+with open(path, encoding='utf-8') as f:
+    for line in f:
+        fruit = line.strip()
+        if fruit:
+            print(f"{fruit} - {len(fruit)} letters")
 
