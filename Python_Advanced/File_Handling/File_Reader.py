4978f9b3936722c473107d8b0287bbb15c6a025e
Danail Donkov <danail.donkov5@gmail.com>
2025-10-08 11:22:53 +0300

    Update File_Reader.py

--- Python_Advanced/File_Handling/File_Reader.py
+++ Python_Advanced/File_Handling/File_Reader.py
@@ -1,1 +1,11 @@
+import os
+
+path = os.path.join(os.getcwd(), 'reading_file')
+print("Търсим файл на пътя:", path)
+
+if os.path.exists(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        print(f.read())
+else:
+    print("File does not exist!")
 
