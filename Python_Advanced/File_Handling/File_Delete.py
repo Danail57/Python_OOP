275122bc49a4e85953b383e7f642834718032768
Danail Donkov <danail.donkov5@gmail.com>
2025-10-08 11:35:22 +0300

    Update File_Delete.py

--- Python_Advanced/File_Handling/File_Delete.py
+++ Python_Advanced/File_Handling/File_Delete.py
@@ -1,1 +1,9 @@
+import os
 
+path = os.path.join(os.getcwd(), 'delete_file.txt')
+
+if os.path.exists(path):
+    os.remove(path)
+    print('File successfully deleted.')
+else:
+    print('File not found.')
