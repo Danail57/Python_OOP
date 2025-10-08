0eb4b407b510d673a8ddb386bf9833b271b4229b
Danail Donkov <danail.donkov5@gmail.com>
2025-10-08 11:11:13 +0300

    Update File_Opener.py

--- Python_Advanced/File_Handling/File_Opener.py
+++ Python_Advanced/File_Handling/File_Opener.py
@@ -1,1 +1,12 @@
+import os
 
+path = os.path.join(os.getcwd(), 'text.txt')
+try:
+    file = open(path, encoding='utf-8')
+    print('File found.')
+    print(file.read())
+    file.close()
+except FileNotFoundError:   
+    print('File not found.')
+
+print(path)
