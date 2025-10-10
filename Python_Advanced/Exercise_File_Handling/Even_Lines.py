a5822d39e91384e9f61f142c0965cc34228418d8
Danail Donkov <danail.donkov5@gmail.com>
2025-10-10 12:33:29 +0300

    Update Even_Lines.py

--- Python_Advanced/Exercise_File_Handling/Even_Lines.py
+++ Python_Advanced/Exercise_File_Handling/Even_Lines.py
@@ -1,1 +1,6 @@
-
+with open("even_lines.txt") as file:
+    for index_row, text_line in enumerate(file):
+        if index_row % 2 == 0:
+            for symbol in {"-", ",", ".", "!", "?"}:
+                text_line = text_line.replace(symbol, "@")
+            print(" ".join(reversed(text_line.split())))
