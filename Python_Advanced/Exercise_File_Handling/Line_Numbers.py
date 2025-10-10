51a9e614bb651a42e8b1489ddc993bf055586d69
Danail Donkov <danail.donkov5@gmail.com>
2025-10-10 12:50:40 +0300

    Update Line_Numbers.py

--- Python_Advanced/Exercise_File_Handling/Line_Numbers.py
+++ Python_Advanced/Exercise_File_Handling/Line_Numbers.py
@@ -1,1 +1,13 @@
+with open("numbers.txt") as input_file, open("output.txt", "w") as output_file:
+    result= []
+    for index_row, text_line in enumerate(input_file):
+        letters_count = 0
+        punctuation_count = 0
 
+        for character in text_line:
+            if character.isalpha():
+                letters_count += 1
+            elif character.isdigit():
+                punctuation_count += 1
+        result.append(f"Line {index_row + 1}: {text_line.strip()} ({letters_count}) ({punctuation_count})")
+    output_file.write("\n".join(result))
