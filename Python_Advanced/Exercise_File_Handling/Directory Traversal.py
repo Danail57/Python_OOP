689b0e96366a14f8d45d3255e04ea7ea3ee2d2df
Danail Donkov <danail.donkov5@gmail.com>
2025-10-10 14:08:21 +0300

    Update Directory Traversal.py

--- Python_Advanced/Exercise_File_Handling/Directory Traversal.py
+++ Python_Advanced/Exercise_File_Handling/Directory Traversal.py
@@ -1,1 +1,24 @@
+import os
+from collections import defaultdict
+
+files = defaultdict(list)
+directory = "."  
+
+for element in os.listdir(directory):
+    path = os.path.join(directory, element)
+    if os.path.isfile(path):
+        ext = os.path.splitext(element)[1]
+        files[ext].append(element)
+    elif os.path.isdir(path):
+        for sub_el in os.listdir(path):
+            sub_path = os.path.join(path, sub_el)
+            if os.path.isfile(sub_path):
+                ext = os.path.splitext(sub_el)[1]
+                files[ext].append(sub_el)
+
+with open("dir_traversal.txt", "w") as report:
+    for ext, filenames in sorted(files.items()):
+        report.write(f"{ext}\n")
+        for name in sorted(filenames):
+            report.write(f"- - - {name}\n")
 
