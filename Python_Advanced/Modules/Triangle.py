38dcd7bd7428a996d788c5e13004001e6d8d3ed1
Danail Donkov <danail.donkov5@gmail.com>
2025-10-22 12:00:58 +0300

    Update Triangle.py

--- Python_Advanced/Modules/Triangle.py
+++ Python_Advanced/Modules/Triangle.py
@@ -1,1 +1,8 @@
+def print_triangle_right(size):
+    for i in range(1, size + 1):
+        print("*" * i)
+    for i in range(size - 1, 0, -1):
+        print("*" * i)
 
+size = int(input("Enter the size of the triangle: "))
+print_triangle_right(size)
