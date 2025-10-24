0323f60abbdec47f9db26b4f3dbb15b94bc14ef9
Danail Donkov <danail.donkov5@gmail.com>
2025-10-24 13:26:57 +0300

    Update Rhombus_of_Stars.py

--- Objects and Classes/First_Steps_in_OOP/Rhombus_of_Stars.py
+++ Objects and Classes/First_Steps_in_OOP/Rhombus_of_Stars.py
@@ -1,1 +1,10 @@
+def print_top(n):
+    for row in range(1, n + 1):
+        print(f"{' '*(n-row)}{'* ' * row}")
 
+def print_bottom(n):
+    for row in range(1, n):
+        print(f"{' '*row}{'* ' * (n-row)}")
+
+print_top(5)
+print_bottom(5)
