7829f8537a8f554aca9f243fabcdc7da5d47d986
Danail Donkov <danail.donkov5@gmail.com>
2025-11-23 12:08:02 +0200

    Update Open_Closed.py

--- Objects and Classes/SOLID/Open_Closed.py
+++ Objects and Classes/SOLID/Open_Closed.py
@@ -28,3 +28,39 @@ class FlyingBird(Bird):
 class Penguin(Bird):
     def swim(self):
         print("Swimming")
+
+#3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+    
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof!"
+    
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
+    
+class Parrot(Dog):
+    def make_sound(self):
+        return "I'm a parrot!"
+
+class Pig(Animal):
+    def make_sound(self):
+        return "Gruh!"
+
+# function is closed for changes
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
+
+animals = [Dog(), Cat(), Parrot()]
+animal_sound(animals)
+
+
