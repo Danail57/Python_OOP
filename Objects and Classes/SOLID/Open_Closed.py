ef452cac8c93f109b2a6906c7a5ff826bd7d762c
Danail Donkov <danail.donkov5@gmail.com>
2025-11-23 12:08:29 +0200

    Refactor Open_Closed.py to use ABC for animals

--- Objects and Classes/SOLID/Open_Closed.py
+++ Objects and Classes/SOLID/Open_Closed.py
@@ -1,35 +1,4 @@
-#1
-class Shape:
-    def area(self):
-        raise NotImplementedError
 
-class Circle(Shape):
-    def __init__(self, r):
-        self.r = r
-    def area(self):
-        return 3.14 * self.r ** 2
-
-class Square(Shape):
-    def __init__(self, a):
-        self.a = a
-    def area(self):
-        return self.a ** 2
-
-
-
-#2
-class Bird:
-    pass
-
-class FlyingBird(Bird):
-    def fly(self):
-        print("Flying")
-
-class Penguin(Bird):
-    def swim(self):
-        print("Swimming")
-
-#3
 from abc import ABC, abstractmethod
 
 
@@ -62,5 +31,3 @@ def animal_sound(animals: list):
 
 animals = [Dog(), Cat(), Parrot()]
 animal_sound(animals)
-
-
