affcf1155159f3f503e6f5c22abaa4d443efc780
Danail Donkov <danail.donkov5@gmail.com>
2025-11-28 10:25:47 +0200

    Update Dictionary_Iterator.py

--- Objects and Classes/Exercise_Iterators_and_Generators/Dictionary_Iterator.py
+++ Objects and Classes/Exercise_Iterators_and_Generators/Dictionary_Iterator.py
@@ -1,1 +1,18 @@
+class dictionary_iter:
+    def __init__(self, dictionary: dict):
+        self.dictionary_tuple = tuple(dictionary.items())
+        self.i = 0
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < len(self.dictionary_tuple):
+            index = self.i
+            self.i += 1
+            return self.dictionary_tuple[index]
+        raise StopIteration
+
+result = dictionary_iter({1: "1", 2: "2"})
+for x in result:
+    print(x)
