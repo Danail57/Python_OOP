305568f7576e8d7b81cd2a05e6633537b323a433
Danail Donkov <danail.donkov5@gmail.com>
2025-11-28 11:46:22 +0200

    Update Reader.py

--- Objects and Classes/Exercise_Iterators_and_Generators/Reader.py
+++ Objects and Classes/Exercise_Iterators_and_Generators/Reader.py
@@ -5,3 +5,8 @@ def read_next(*args):
 for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
     print(item, end='')
 
+def read_next(*args):
+    for sequence in args:
+        # for item in sequence:
+        #     yield item
+        yield from sequence
