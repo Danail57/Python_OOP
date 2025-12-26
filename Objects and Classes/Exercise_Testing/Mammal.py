d6111c3a6b4fa1d0720f81a31d04d86e9bd9a0c3
Danail Donkov <danail.donkov5@gmail.com>
2025-12-26 20:30:55 +0200

    Update Mammal.py

--- Objects and Classes/Exercise_Testing/Mammal.py
+++ Objects and Classes/Exercise_Testing/Mammal.py
@@ -1,1 +1,44 @@
+class Mammal:
+    def __init__(self, name, mammal_type, sound):
+        self.name = name
+        self.type = mammal_type
+        self.sound = sound
+        self.__kingdom = "animals"
 
+    def make_sound(self):
+        return f"{self.name} makes {self.sound}"
+
+    def get_kingdom(self):
+        return self.__kingdom
+
+    def info(self):
+        return f"{self.name} is of type {self.type}"
+
+
+
+from unittest import TestCase, main
+
+class MammalTest(TestCase):
+    def setUp(self):
+        self.mammal = Mammal("Test", "Test Type", "Test Sound")
+
+    def test_init(self):
+        self.assertEqual("Test", self.mammal.name)
+        self.assertEqual("Test Type", self.mammal.type)
+        self.assertEqual("Test Sound", self.mammal.sound)
+        self.assertEqual("animals", self.mammal._Mammal__kingdom)
+
+    def test_make_sound(self):
+        result = self.mammal.make_sound()
+        self.assertEqual("Test makes Test Sound", result)
+
+    def test_get_kingdom(self):
+        result = self.mammal.get_kingdom()
+        self.assertEqual("animals", result)
+
+    def test_info(self):
+        result = self.mammal.info()
+        self.assertEqual(f"Test is of type Test Type", result)
+
+if __name__ == '__main__':
+    main()
