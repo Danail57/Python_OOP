fcd60ebfe5ad9becacc0d20d03100eb6a39fa6d8
Danail Donkov <danail.donkov5@gmail.com>
2025-12-30 12:01:27 +0200

    Update Singleton.py

--- Objects and Classes/Exercise_Design_Patterns/Singleton.py
+++ Objects and Classes/Exercise_Design_Patterns/Singleton.py
@@ -1,1 +1,27 @@
+class Singleton:
+     __instance = None
 
+     def __init__(self):
+         if Singleton.__instance is not None:
+             raise Exception("This class is a singleton.")
+
+     @classmethod
+     @property
+     def get_instance(cls):
+         if cls.__instance is None:
+             cls.__instance = cls()
+         return cls.__instance
+
+s1 = Singleton.get_instance
+s2 = Singleton.get_instance
+
+print(s1 is s2)
+print(s1)
+print(s2)
+if s1 == s2:
+    print("Same")
+else:
+    print("Different")
+
+print(id(s1))
+print(id(s2))
