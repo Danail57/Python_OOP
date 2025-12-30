74d764f741f9712a988f2974313d3e1611db450c
Danail Donkov <danail.donkov5@gmail.com>
2025-12-30 18:48:57 +0200

    Create Abstract_Factory_Furniture.py

---
+++ Objects and Classes/Exercise_Design_Patterns/Abstract_Factory_Furniture.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractFactory(ABC):
+    @abstractmethod
+    def create_chair(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def create_sofa(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def create_table(self):
+        raise NotImplementedError()
+
+class Chair:
+    def __init__(self, name):
+        self._name = name
+
+    def __str__(self):
+        return self._name
+
+class Sofa:
+    def __init__(self, name):
+        self._name = name
+    def __str__(self):
+        return self._name
+
+class Table:
+    def __init__(self, name):
+        self._name = name
+    def __str__(self):
+        return self._name
+
+class VictorianFactory(AbstractFactory):
+    def create_chair(self):
+        return Chair("Victorian chair")
+    def create_sofa(self):
+        return Sofa("Victorian sofa")
+    def create_table(self):
+        return Table("Victorian table")
+
+class ModernFactory(AbstractFactory):
+    def create_chair(self):
+        return Chair("Modern chair")
+    def create_sofa(self):
+        return Sofa("Modern sofa")
+    def create_table(self):
+        return Table("Modern table")
+
+class FuturisticFactory(AbstractFactory):
+    def create_chair(self):
+        return Chair("Futuristic chair")
+    def create_sofa(self):
+        return Sofa("Futuristic sofa")
+    def create_table(self):
+        return Table("Futuristic table")
+
+def assemble_furniture(factory: AbstractFactory):
+    chair = factory.create_chair()
+    sofa = factory.create_sofa()
+    table = factory.create_table()
+
+    print("Furniture set:")
+    print("-", chair)
+    print("-", sofa)
+    print("-", table)
+factory = FuturisticFactory()
+assemble_furniture(factory)
