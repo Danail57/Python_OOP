d08abec8cc60d5f7ac0c2ddddf1594e6f6db999c
Danail Donkov <danail.donkov5@gmail.com>
2025-12-30 13:14:59 +0200

    Update Abstract_Factory.py

--- Objects and Classes/Exercise_Design_Patterns/Abstract_Factory.py
+++ Objects and Classes/Exercise_Design_Patterns/Abstract_Factory.py
@@ -1,1 +1,45 @@
+from abc import ABC, abstractmethod
+import json
+
+class JsonDataExporter(ABC):
+    @abstractmethod
+    def export(self, data) -> str:
+        pass
+
+class CsvDataExporter(ABC):
+    @abstractmethod
+    def export(self, data) -> str:
+        pass
+
+class DataExporterFactory(ABC):
+    @abstractmethod
+    def get_json_exporter(self) -> JsonDataExporter:
+        pass
+
+    @abstractmethod
+    def get_csv_exporter(self) -> CsvDataExporter:
+        pass
+
+class SimpleJsonDataExporter(CsvDataExporter):
+    def export(self, data) -> str:
+        return json.dumps(data)
+
+class SimpleCsvDataExporter(CsvDataExporter):
+    def export(self, data) -> str:
+        return "presenting data in CSV"
+
+class SimpleDataExporterFactory(DataExporterFactory):
+    def get_json_exporter(self) -> JsonDataExporter:
+        return SimpleJsonDataExporter()
+
+    def get_csv_exporter(self) -> CsvDataExporter:
+        return SimpleCsvDataExporter()
+
+factory = SimpleDataExporterFactory()
+json_exporter = factory.get_json_exporter()
+csb_exporter = factory.get_csv_exporter()
+
+data = {"name": "Ivan", "age": 25}
+print(json_exporter.export(data))
+print(csb_exporter.export(data))
 
